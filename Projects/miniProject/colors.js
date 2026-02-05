1c97a7c748b573942b259c97cb5485a28a0b7ffd
Sandesh Dadibude <sandeshdadibude28042004@.com>
2026-02-05 22:26:54 +0530

    understand the working of the modules in node

---
+++ Projects/miniProject/colors.js
@@ -0,0 +1,7 @@
+const colors=require("colors");
+console.log(colors.red("Hii,Its me sandesh"));
+console.log(colors.green("Hii,Its me sandesh"));
+
+console.log(colors.white("Hii,Its me sandesh"));
+console.log(colors.blue("Hii,Its me sandesh"));
+console.log(colors.bgCyan("Hii,Its me sandesh"));
